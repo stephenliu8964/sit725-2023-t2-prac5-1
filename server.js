30d839bd66d2eb7c058a9ca8f0aa36a6ae8068fa
stephenliu8964 <dq89_64@hotmail.com>
2023-08-01 18:13:42 +1000

    workshop 2

--- server.js
+++ server.js
@@ -1,21 +1,63 @@
 let express = require('express');
 let app = express();
 let port = process.env.port || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:admin@cluster0.l9hmwv8.mongodb.net/?retryWrites=true&w=majority";
+let collection;
 
 app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+async function run() {
+    try {
+      await client.connect();
+      collection = client.db().collection('Cats');
+      console.log(collection);
+    } catch(ex) {
+      console.error(ex);
+    }
+}
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+});
 
 app.get('/', function (req,res) {
     res.render('index.html');
 });
 
-app.get('/addTwoNumbers', function (req,res) {
-    let num1 = req.query.num1;
-    let num2 = req.query.num2;
-    let result = parseInt(num1) + parseInt(num2);
-    let response = {data:result, message:'sucess', statusCode:200}
-    res.json(response);
+app.post('/api/cat', function(req,res){
+    // ask DB to post this CAT
+    let cat = req.body;
+    insertCat(cat, (err,result) => {
+        if (!err) {
+            res.json({statusCode:201,data:result,message:'success'});
+        }
+    });
 });
+
+function insertCat(cat, callback) {
+    collection.insertOne(cat,callback);
+}
+
+app.get('/api/cats', (req,res)=>{
+    getAllCats((err,result)=>{
+        if (!err) {
+            res.json({statusCode:200,data:result,message:'success'});
+        }
+    });
+});
+
+function getAllCats(callback) {
+    collection.find({}).toArray(callback);
+}
 
 app.listen(port, ()=>{
     console.log('express server started');
+    run();
 });
